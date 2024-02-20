debbddd20c51efb4815a6469be85ac72a6957847
Nastenka <nastenka@Nastenkas-MacBook-Air.local>
2024-02-20 23:06:07 +0300

    feat: create header

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
       fontFamily: {
         instument: ["Instrument Sans", "sans-serif"],
       },
+      animation: {
+        'open-menu': 'open-menu .5s ease-out'
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
     },
   },
   plugins: [],
